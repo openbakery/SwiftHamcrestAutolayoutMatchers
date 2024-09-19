491913d8a8e13100808a5f1967ed0994815a52b9
René Pirringer <rene.pirringer@ciqua.com>
2024-09-19 10:37:52 +0200

    pinned version to 2.2.4

--- Package.swift
+++ Package.swift
@@ -1,41 +1,41 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.9
 
 
 import PackageDescription
 
 let package = Package(
-	name: "HamcrestAutolayoutMatchers",
-	platforms: [
-		.iOS(.v14),
-	],
-	products: [
-		.library(name: "HamcrestAutolayoutMatchers", targets: ["HamcrestAutolayoutMatchers"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/nschum/SwiftHamcrest", .exact("2.2.4")),
-		.package(url: "https://github.com/openbakery/PinLayout", .branch("main")),
-	],
-	targets: [
-		.target(
-			name: "HamcrestAutolayoutMatchers",
-			dependencies: [
-				.product(name: "Hamcrest", package: "SwiftHamcrest"),
-			],
-			path: "Matchers",
-			sources: [
-				"Main/Source"
-			]
-		),
-		.testTarget(
-			name: "HamcrestAutolayoutMatchersTest",
-			dependencies: [
-				"HamcrestAutolayoutMatchers",
-				"PinLayout"
-			],
-			path: "Matchers",
-			sources: [
-				"Test/Source"
-			]
-		),
-	]
+    name: "HamcrestAutolayoutMatchers",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "HamcrestAutolayoutMatchers", targets: ["HamcrestAutolayoutMatchers"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nschum/SwiftHamcrest/", .exact("2.2.4")),
+        .package(url: "https://github.com/openbakery/PinLayout", .branch("main"))
+    ],
+    targets: [
+        .target(
+            name: "HamcrestAutolayoutMatchers",
+            dependencies: [
+                .product(name: "Hamcrest", package: "SwiftHamcrest")
+            ],
+            path: "Matchers",
+            sources: [
+                "Main/Source"
+            ]
+        ),
+        .testTarget(
+            name: "HamcrestAutolayoutMatchersTest",
+            dependencies: [
+                "HamcrestAutolayoutMatchers",
+                "PinLayout"
+            ],
+            path: "Matchers",
+            sources: [
+                "Test/Source"
+            ]
+        )
+    ]
 )
